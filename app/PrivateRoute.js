901dd9d458ef1482950836dd71f4fb6d57e3c3e1
Anant Jain <ritujainanant2810@gmail.com>
2024-06-28 22:15:45 +0530

    location error 2

--- app/PrivateRoute.js
+++ app/PrivateRoute.js
@@ -1,14 +1,38 @@
 
+// import { useSelector } from "react-redux";
+// import { useRouter } from "next/navigation";
+
+// const PrivateRoute = ({ children }) => {
+//   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+//   const router = useRouter();
+
+//   if (!isAuthenticated) {
+//     router.push("/login");
+//     return null;
+//   }
+
+//   return children;
+// };
+
+// export default PrivateRoute;
+
+"use client"
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const PrivateRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const router = useRouter();
 
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login");
+    }
+  }, [isAuthenticated, router]);
+
   if (!isAuthenticated) {
-    router.push("/login");
-    return null; 
+    return null; // Or you could return a loading indicator or a fallback UI
   }
 
   return children;
